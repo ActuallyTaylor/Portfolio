390b21cac945087601df4836270a028bafb8db31
Zachary Lineman <zachary.lineman@gmail.com>
2022-10-25 20:44:39 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,19 +7,9 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
-    kit: {
-        // default options are shown
-        adapter: adapter({
-          // if true, will create a Netlify Edge Function rather
-          // than using standard Node-based functions
-          edge: false,
-    
-          // if true, will split your app into multiple functions
-          // instead of creating a single one for the entire app.
-          // if `edge` is true, this option cannot be used
-          split: false
-        })
-      }
-  };
+	kit: {
+		adapter: adapter()
+	}
+};
 
-export default config;  
+export default config;
