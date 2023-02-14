7265c9137a0ff5e0261fccb470ac765b0591d13b
Zachary Lineman <zachary.lineman@gmail.com>
2023-02-14 11:08:36 -0500

    Fixed an image bug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
-  <link rel="icon" type="image/png" href="assets/icon.png" />
-  <title>Zachary Lineman</title>
+    <link rel="icon" href="/favicon.png" />
+    <title>Zachary Lineman</title>
   <meta name="description" content="My personal website, showcasing my interests, projects and blog.">
 </svelte:head>
 <slot>
