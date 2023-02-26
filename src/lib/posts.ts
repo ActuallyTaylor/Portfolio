6c3d2bbfbe0a5c108384e7e4da6cc7a000e851db
Zachary Lineman <zachary.lineman@gmail.com>
2023-02-25 20:44:48 -0500

    Update posts.ts

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -5,20 +5,20 @@ import type { BlogEntry } from './models/BlogEntry';
 export function readPosts(): BlogEntry[] {
     let blogs: BlogEntry[] = []
 
-    readdirSync("/posts").forEach((file) => {
-        let readFile = readFileSync(`./static/posts/${file}`)
-        let frontmatter = fm(readFile.toString());
-        let attributes = frontmatter.attributes
-        let title = attributes.title;
-        let slug = attributes.slug;
-        let description = attributes.description;
-        let author = attributes.author;
-        let date = new Date(attributes.date);
-        let hex = `#${attributes.hex}`
-        let content = frontmatter.body
+    // readdirSync("/posts").forEach((file) => {
+    //     let readFile = readFileSync(`./static/posts/${file}`)
+    //     let frontmatter = fm(readFile.toString());
+    //     let attributes = frontmatter.attributes
+    //     let title = attributes.title;
+    //     let slug = attributes.slug;
+    //     let description = attributes.description;
+    //     let author = attributes.author;
+    //     let date = new Date(attributes.date);
+    //     let hex = `#${attributes.hex}`
+    //     let content = frontmatter.body
         
-        blogs.push({title, slug, description, author, date, hex, content})
-    });      
+    //     blogs.push({title, slug, description, author, date, hex, content})
+    // });      
 
     return blogs
 }
