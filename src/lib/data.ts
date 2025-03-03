2aa1b3326fe242053e3b82320733d6df1e7a6fb0
Taylor Lineman <taylor.lineman@gmail.com>
2025-03-03 13:02:05 -0500

    Fixed a navigation bu

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,8 +1,6 @@
 import type { Project } from "./models/Project";
-import worksJSON from './data/works.json'
-import interestsJSON from './data/interests.json'
-import openSourceJSON from './data/openSource.json'
+import worksJSON from "./data/works.json";
+import openSourceJSON from "./data/openSource.json";
 
 export let works: Project[] = worksJSON;
-export let openSource: Project[] =  openSourceJSON;
-export let interests: Project[] = interestsJSON;
+export let openSource: Project[] = openSourceJSON;
