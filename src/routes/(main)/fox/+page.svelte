4cf869de5d76ead289a1ebf3d60c4b14a0a621cd
Taylor Lineman <git@actuallytaylor.com>
2025-11-10 16:19:26 -0500

    Add 🦊
    Signed-off-by: Taylor Lineman <git@actuallytaylor.com>

---
+++ src/routes/(main)/fox/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+    var imageURL = ""
+
+    async function newFox() {
+        var api = "https://randomfox.ca/floof/"
+
+        var response = await fetch(api)
+        const result = await response.json();
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        imageURL = result['image']
+    }
+
+    newFox()
+</script>
+
+<h1>Fox 🦊</h1>
+<p>The result of a 15 minute hackathon in my SWEN-440 class. We had to use an un-authenticated API from the <a href="https://github.com/public-api-lists/public-api-lists">public-api-lists</a> GitHub page.</p>
+
+<div class="container">
+    <button on:click={ newFox }>
+        Get a new 🦊
+    </button>
+    <img id="fox" alt="A random a fox." src={imageURL}>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        gap: 10px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    button {
+        background-color: transparent;
+        border: 1px solid black;
+    }
+
+    img {
+        max-width: 90%;
+        max-height: 50vh;
+        box-shadow: 0px 0px 20px #00000084;
+        border-radius: 10px;
+    }
+
+    a {
+        text-decoration: underline;
+    }
+</style>
