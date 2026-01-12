351c092b735808b242ccd7cf3b04a9d1b7a042a4
Taylor Lineman <git@actuallytaylor.com>
2026-01-12 15:53:20 -0500

    Fix my bad decision to use string manipultion for xml.
    Signed-off-by: Taylor Lineman <git@actuallytaylor.com>

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,94 +1,73 @@
-import { readdirSync, readFileSync } from "fs";
+import {create} from 'xmlbuilder2';
+import {readdirSync, readFileSync} from "fs";
 import fm from "front-matter";
-import type { BlogEntry } from "./models/BlogEntry";
+import type {BlogEntry} from "./models/BlogEntry";
 
 export function readPosts(): BlogEntry[] {
-  let blogs: BlogEntry[] = [];
+    let blogs: BlogEntry[] = [];
 
-  readdirSync("./static/posts/").forEach((file) => {
-    if (file.includes(".md")) {
-      let readFile = readFileSync(`./static/posts/${file}`);
-      let frontmatter = fm(readFile.toString());
-      let attributes = frontmatter.attributes;
-      let title = attributes.title;
-      let slug = attributes.slug;
-      let description = attributes.description;
-      let author = attributes.author;
-      let readingTime = attributes.readingTime;
-      let series = attributes.series;
+    readdirSync("./static/posts/").forEach((file) => {
+        if (file.includes(".md")) {
+            let readFile = readFileSync(`./static/posts/${file}`);
+            let frontmatter = fm(readFile.toString());
+            let attributes = frontmatter.attributes;
 
-      let date = new Date(attributes.date);
-      let hex = `#${attributes.hex}`;
-      let content = frontmatter.body;
+            //@ts-ignore
+            let {title, slug, description, author, readingTime, series} = attributes;
 
-      blogs.push({
-        title,
-        slug,
-        series,
-        readingTime,
-        description,
-        author,
-        date,
-        hex,
-        content,
-      });
-    }
-  });
+            //@ts-ignore
+            let date = new Date(attributes.date);
 
-  blogs.sort(function (a, b): any {
-    return b.date.getTime() - a.date.getTime();
-  });
+            //@ts-ignore
+            let hex = `#${attributes.hex}`;
 
-  return blogs;
+            let content = frontmatter.body;
+
+            blogs.push({
+                title,
+                slug,
+                series,
+                readingTime,
+                description,
+                author,
+                date,
+                hex,
+                content,
+            });
+        }
+    });
+
+    blogs.sort(function (a, b): any {
+        return b.date.getTime() - a.date.getTime();
+    });
+
+    return blogs;
 }
 
 export function createRSSFeed(): string {
-  const title = escapeXml("Taylor Lineman");
-  const description = escapeXml("");
-  const siteURL = escapeXml("https://actuallytaylor.com");
+    const title = "Taylor Lineman";
+    const siteURL = "https://actuallytaylor.com";
 
-  let feedItems = "";
+    const root = create({version: '1.0'})
+        .ele('rss', {
+            version: '2.0',
+            'xmlns:atom': 'http://www.w3.org/2005/Atom',
+            'xmlns:dc': 'http://purl.org/dc/elements/1.1/'
+        })
+        .ele('channel')
+        .ele('title').txt(title).up()
+        .ele('subtitle').txt("Experience the web with me!").up()
+        .ele('link').txt(siteURL + '/blog/rss').up()
+        .ele('atom:link', {href: siteURL + '/blog/rss', rel: 'self', type: 'application/rss+xml'}).up();
 
-  readPosts().forEach((element) => {
-    feedItems += `
-    <item>
-        <title>${element.title}</title>
-        <description>${element.description}</description>
-        <pubDate>${element.date.toUTCString()}</pubDate>
-        <link>${siteURL}/blog/${escapeXml(element.slug)}</link>
-        <guid isPermaLink="true">${siteURL}/blog/${escapeXml(element.slug)}</guid>
-    </item>
-        `;
-  });
+    readPosts().forEach((element) => {
+        root.ele('item')
+            .ele('title').txt(element.title).up()
+            .ele('description').txt(element.description).up()
+            .ele('pubDate').txt(element.date.toUTCString()).up()
+            .ele('link').txt(siteURL + '/blog/' + element.slug).up()
+            .ele('guid', {isPermaLink: 'true'}).txt(siteURL + '/blog/' + element.slug).up()
+    })
 
-  let rss = `<?xml version="1.0" encoding="UTF-8"?>
-    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
-    <channel>
-        <title>${title}</title>
-        <subtitle>Experience the web with me!</subtitle>
-        <link>${siteURL}/blog/rss</link>
-        <atom:link href="${siteURL}/blog/rss" rel="self" type="application/rss+xml" />
-        ${feedItems}
-    </channel>
-    </rss>
-    `;
-
-  return rss;
-}
-
-function escapeXml(unsafe: string) {
-  return unsafe.replace(/[<>&'"]/g, function (c) {
-    switch (c) {
-      case "<":
-        return "&lt";
-      case ">":
-        return "&gt";
-      case "&":
-        return "&amp";
-      case '"':
-        return "&quot";
-      case "'":
-        return "&apos";
-    }
-  });
+    return root.end({prettyPrint: true});
 }
