01c2ffed10a807f5a4ac4fada17c2f63bfe30595
Taylor Lineman <git@actuallytaylor.com>
2026-05-05 16:38:19 -0400

    Add page entries for blogs for consistency
    Signed-off-by: Taylor Lineman <git@actuallytaylor.com>

--- src/routes/(main)/blog/[slug]/+page.server.ts
+++ src/routes/(main)/blog/[slug]/+page.server.ts
@@ -1,10 +1,16 @@
 import type {BlogEntry} from '$lib/models/BlogEntry'
 import {readPosts} from '$lib/posts'
 import {error, type RequestEvent} from '@sveltejs/kit'
-import type { OpenGraph } from "$lib/models/OpenGraph";
+import type { EntryGenerator } from './$types';
 
 // We need to prerender here because it is using filesystem operations that only work on Netlify if they are pre-rendered as Netlify does not give you a server to run your SvelteKit site on.
 export const prerender = true
+
+// Inform SvelteKit about all the slugs it should pre-render. Modified from: https://sveltekit-og.dev/docs/advanced-usage/pre-rendering
+export const entries: EntryGenerator = () => {
+    const blogs = readPosts().map((blog) => blog.slug)
+	return blogs.map((slug) => ({ slug: slug }));
+};
 
 export async function load(event: RequestEvent): Promise<BlogEntry> {
     if (typeof event.params.slug !== 'string') {
